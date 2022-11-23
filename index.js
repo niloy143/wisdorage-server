5a2500d311c6e7c14afad1c4f8b433b08df6338f
niloy143 <niloymahmudapu@gmail.com>
2022-11-24 00:14:33 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 1234;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send({ status: 'running' })
+})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sq5icdb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const usersCollection = client.db('wisdorage').collection('users');
+        app.get('/users', async (req, res) => {
+            
+        })
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+run().catch(err => console.log(err))
+
+app.listen(port, () => {
+    console.log(`Wisdorage server is running on ${port}`)
+})
