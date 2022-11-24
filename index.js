40a432beaf874607cff3772447630638892742f2
niloy143 <niloymahmudapu@gmail.com>
2022-11-25 00:49:31 +0600

    jwt sign

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 1234;
+const jwtSecret = process.env.JWT_SECRET;
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send({ status: 'running' })
+})
+
+app.get('/jwt', (req, res) => {
+    const token = jwt.sign({ email: req.query.email }, jwtSecret, { expiresIn: '7d' });
+    res.send({ token });
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sq5icdb.mongodb.net/?retryWrites=true&w=majority`;
